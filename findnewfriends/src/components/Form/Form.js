5654e7e24664837d271bb5d0f2f0715dee224a3b
Bryce Rothhammer <rothhammer.b@gmail.com>
2018-01-19 17:35:09 -0600

    added login form, next need a component to create new user

---
+++ findnewfriends/src/components/Form/Form.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import "./Form.css";
+
+class Form extends Component {
+  // Setting the component's initial state
+  state = {
+    firstName: "",
+    lastName: "",
+    password: ""
+  };
+
+  handleInputChange = event => {
+    // Getting the value and name of the input which triggered the change
+    let value = event.target.value;
+    const name = event.target.name;
+
+    if (name === "password") {
+      value = value.substring(0, 15);
+    }
+    // Updating the input's state
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    event.preventDefault();
+    if (!this.state.firstName || !this.state.lastName) {
+      alert("Fill out your first and last name please!");
+    } else if (this.state.password.length < 6) {
+      alert(
+        `Choose a more secure password ${this.state.firstName} ${this.state
+          .lastName}`
+      );
+    } else {
+      alert(`Hello ${this.state.firstName} ${this.state.lastName}`);
+    }
+
+    this.setState({
+      firstName: "",
+      lastName: "",
+      password: ""
+    });
+  };
+
+}
+
+export default Form;
--- findnewfriends/src/components/homepageContainer.js
+++ findnewfriends/src/components/homepageContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import AppBarExampleIcon from './AppBar';
+import Form from "./Form";
 
 class HomepageContainer extends Component {
   state = {
@@ -8,7 +9,42 @@ class HomepageContainer extends Component {
   };
 
 render() {
-	return (<AppBarExampleIcon/>)
+	return (
+
+		<div>
+
+			<AppBarExampleIcon/>
+			
+	        <p>
+	          Login {this.state.firstName} {this.state.lastName}
+	        </p>
+	        <form className="form">
+	          <input
+	            value={this.state.firstName}
+	            name="firstName"
+	            onChange={this.handleInputChange}
+	            type="text"
+	            placeholder="First Name"
+	          />
+	          <input
+	            value={this.state.lastName}
+	            name="lastName"
+	            onChange={this.handleInputChange}
+	            type="text"
+	            placeholder="Last Name"
+	          />
+	          <input
+	            value={this.state.password}
+	            name="password"
+	            onChange={this.handleInputChange}
+	            type="password"
+	            placeholder="Password"
+	          />
+	          <button onClick={this.handleFormSubmit}>Submit</button>
+	          <button>Create New User</button>
+	        </form>
+      	</div>
+	);
 }
 }
 

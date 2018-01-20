9678d33f19002ec6a5f29664de086bd5556cb0b7
Lauren Cowen <lauren.cowen@gmail.com>
2018-01-20 10:57:47 -0600

    added encryption to the passwords

--- findnewfriends/models/Profile.js
+++ findnewfriends/models/Profile.js
@@ -1,5 +1,12 @@
 // Require mongoose
 var mongoose = require("mongoose");
+//npm packages for validating urls and emails with Mongoose
+require('mongoose-type-url');
+require('mongoose-type-email');
+//bycrypt for encrypting passwords
+bcrypt = require('bcrypt'),
+SALT_WORK_FACTOR = 10;
+
 
 // Create a schema class using mongoose's schema method
 var Schema = mongoose.Schema;
@@ -19,6 +26,13 @@ var profileSchema = new Schema({
   eMail: {
     type: mongoose.SchemaTypes.Email,
     required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  imageURL: {
+    type: mongoose.SchemaTypes.Url
   },
   // date is just a string
   generation: {
@@ -42,6 +56,35 @@ var profileSchema = new Schema({
   	default: []
   }
 });
+
+profileSchema.pre('save', function(next){
+  var user = this;
+
+  //only hash the password if it has been modified or is new
+  if(!user.isModified('password')) return next();
+
+  //generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt){
+    if(err) return next (err);
+
+    //hash the password using our new salt
+    bcrypt.hash(user.password, salt, function(err, salt){
+      if(err) return next(err);
+
+      //override the cleartext password with the hashed one
+      user.password = hash;
+      next()
+    });
+  });
+
+});
+
+profileSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
+    if(err) return cb(err);
+    cb(null, isMatch);
+  });
+};
 
 // Create the Profile model using the headlineSchema
 var Profile = mongoose.model("Profile", profileSchema);
